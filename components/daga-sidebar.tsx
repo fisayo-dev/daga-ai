cec3a3086b0deab1f4612603fe8da941796b0a29
fisayo-dev <olufisayobadina@gmail.com>
2025-10-01 14:48:18 +0100

    style: customizing sidebar look

--- components/daga-sidebar.tsx
+++ components/daga-sidebar.tsx
@@ -1,4 +1,4 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+import { AtomIcon, Calendar, Home, Inbox, Search, Settings } from "lucide-react"
 
 import {
   Sidebar,
@@ -9,7 +9,9 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  SidebarTrigger,
 } from "@/components/ui/sidebar"
+import Link from "next/link"
 
 // Menu items.
 const items = [
@@ -45,21 +47,28 @@ function AppSidebar() {
     <Sidebar>
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
+            <div className="px-2 py-10 flex justify-between">
+                <div className="flex items-center gap-2">
+                    <AtomIcon />
+                    <h2 className="text-xl font-bold">Daga AI</h2>
+                </div>
+                <SidebarTrigger />
+            </div>
+            
+            <SidebarGroupContent>
+                <SidebarMenu className="px-2">
+                {items.map((item) => (
+                    <SidebarMenuItem key={item.title}>
+                        <SidebarMenuButton asChild  className="p-4 h-12">
+                            <Link href={item.url}>
+                                <item.icon />
+                                <span>{item.title}</span>
+                            </Link>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                ))}
+                </SidebarMenu>
+            </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
     </Sidebar>
